5d0344c593831e9a86083ecc5fed049bb9d41204
guttergainz <155899448+guttergainz@users.noreply.github.com>
2024-03-11 13:59:02 +0100

    Update linea.sh

--- linea.sh
+++ linea.sh
@@ -29,16 +29,14 @@ echo "Genesis file downloaded."
 cd "/root/${DIR_NAME}"
 
 # Create a Dockerfile
-cat > "/root/${DIR_NAME}/Dockerfile" << EOF
+cat > "Dockerfile" << EOF
 FROM ubuntu:latest
-WORKDIR /root/${DIR_NAME}/linea
-RUN ls
-RUN ls /root/${DIR_NAME}/linea
-COPY genesis.json /root/${DIR_NAME}/linea/genesis.json
-RUN mkdir /root/${DIR_NAME}/linea/linea_data && \
-    geth --datadir /root/${DIR_NAME}/linea/linea_data init /root/${DIR_NAME}/linea/genesis.json
+WORKDIR linea
+COPY genesis.json /linea/genesis.json
+RUN mkdir /linea/linea_data && \
+    geth --datadir /linea/linea_data init /linea/genesis.json
 EXPOSE 8627 8628 30305
-CMD ["geth", "--datadir", "/root/${DIR_NAME}/linea/linea_data", "--networkid", "59144", "--rpc.allow-unprotected-txs", "--txpool.accountqueue", "50000", "--txpool.globalqueue", "50000", "--txpool.globalslots", "50000", "--txpool.pricelimit", "1000000", "--txpool.pricebump", "1", "--txpool.nolocals", "--http", "--http.addr", "0.0.0.0", "--http.port", "8627", "--http.corsdomain", "*", "--http.api", "web3,eth,txpool,net", "--http.vhosts", "*", "--ws", "--ws.addr", "0.0.0.0", "--ws.port", "8628", "--ws.origins", "*", "--ws.api", "web3,eth,txpool,net", "--bootnodes", "enode://...", "--discovery.port", "30305", "--port", "30305", "--syncmode", "full", "--metrics", "--verbosity", "3"]
+CMD ["geth", "--datadir", "/linea/linea_data", "--networkid", "59144", "--rpc.allow-unprotected-txs", "--txpool.accountqueue", "50000", "--txpool.globalqueue", "50000", "--txpool.globalslots", "50000", "--txpool.pricelimit", "1000000", "--txpool.pricebump", "1", "--txpool.nolocals", "--http", "--http.addr", "0.0.0.0", "--http.port", "8627", "--http.corsdomain", "*", "--http.api", "web3,eth,txpool,net", "--http.vhosts", "*", "--ws", "--ws.addr", "0.0.0.0", "--ws.port", "8628", "--ws.origins", "*", "--ws.api", "web3,eth,txpool,net", "--bootnodes", "enode://...", "--discovery.port", "30305", "--port", "30305", "--syncmode", "full", "--metrics", "--verbosity", "3"]
 EOF
 
 # Build the Docker image
