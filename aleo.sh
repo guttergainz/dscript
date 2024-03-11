a4e056a381fd44a57b1dc694a75597828e8ec845
guttergainz <155899448+guttergainz@users.noreply.github.com>
2024-03-11 17:44:22 +0100

    Update aleo.sh

--- aleo.sh
+++ aleo.sh
@@ -10,7 +10,7 @@ cd "/root/${DIR_NAME}"
 # Create a Dockerfile
 cat > Dockerfile << EOF
 FROM rust:latest
-RUN apt-get update && apt-get upgrade -y && apt-get install -y git
+RUN apt-get update && apt-get upgrade -y && apt-get install -y git curl
 WORKDIR /aleo
 RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 RUN source "$HOME/.cargo/env"
